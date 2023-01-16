bfca78496b9e1ff5b2eeec2143fc1ff1ca6e6aa0
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2023-01-16 11:40:20 +0000

    Created setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='detDyn',
+      version='0.1',
+      description='Computations in nonlinear dynamics.',
+      author='Calvin Nesbitt',
+      author_email='cf.nesbitt95@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
